16e69b55881abc27b1240f3b1c19113745f76a19
Jan Alexander Steffens (heftig) <jan.steffens@gmail.com>
2020-01-27 18:11:05 +0100

    ZEN: INTERACTIVE: Use BFQ as our elevator
    Add a scheduler even to multi-queue block devices: We prefer
    interactivity to throughput and want BFQ if possible.
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- block/elevator.c
+++ block/elevator.c
@@ -633,6 +633,9 @@ static inline bool elv_support_iosched(struct request_queue *q)
  */
 static struct elevator_type *elevator_get_default(struct request_queue *q)
 {
+#if defined(CONFIG_IOSCHED_BFQ)
+	return elevator_get(q, "bfq", false);
+#else
 	if (q->tag_set && q->tag_set->flags & BLK_MQ_F_NO_SCHED_BY_DEFAULT)
 		return NULL;
 
@@ -641,6 +644,7 @@ static struct elevator_type *elevator_get_default(struct request_queue *q)
 		return NULL;
 
 	return elevator_get(q, "mq-deadline", false);
+#endif
 }
 
 /*
