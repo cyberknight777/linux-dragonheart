d731f92ad2133f896109dedfd206bafd7c9c2896
Cyber Knight <cyberknight755@gmail.com>
2022-02-10 14:37:58 +0800

    ul: Add TG notify snippet
    Co-authored-by: PeroSar <perosar1111@gmail.com>
    Signed-off-by: PeroSar <perosar1111@gmail.com>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- ul.sh
+++ ul.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
-str=$(
-	for f in $(find . -maxdepth 1 -type f -iname "*.pkg.tar.zst"); do
-		echo "-F f[]=@${f}"
-	done
-)
-curl -i $str https://oshi.at
+
+CHATID=-1001237208416
+
+for f in $(find . -maxdepth 1 -type f -iname "*.pkg.tar.zst"); do
+    url=$(curl -F f[]=@"${f}" https://oshi.at | grep "DL:" | cut -d ' ' -f2)
+    URLS+=("$url")
+done
+
+curl -sX POST https://api.telegram.org/bot"${TOKEN}"/sendMessage \
+    -d chat_id="${CHATID}" \
+    -d parse_mode=HTML \
+    -d disable_web_page_preview=true \
+    -d text="
+<b>linux-dragonheart CI Build Completed</b>
+
+Kernel:  ${URLS[1]}
+Headers: ${URLS[0]}
+
+Install with <code>pacman -U linux-dragonheart-*</code>"
