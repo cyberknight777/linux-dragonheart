15ba9e5f4773d122d1d44b4139c6bd449a0b362b
Cyber Knight <cyberknight755@gmail.com>
2022-04-29 02:38:28 +0800

    LKS: Merge tag `v5.17.5-arch1` of `archlinux/linux`
    Arch Linux kernel v5.17.5-arch1
    
    * tag 'v5.17.5-arch1' of https://github.com/archlinux/linux: (1845 commits)
      Arch Linux kernel v5.17.5-arch1
      NFSv4.1 provide mount option to toggle trunking discovery
      lib/irq_poll: Declare IRQ_POLL softirq vector as ksoftirqd-parking safe
      tick/rcu: Stop allowing RCU_SOFTIRQ in idle
      tick/rcu: Remove obsolete rcu_needs_cpu() parameters
      tick: Detect and fix jiffies update stall
      random: treat bootloader trust toggle the same way as cpu trust toggle
      ZEN: Add sysctl and CONFIG to disallow unprivileged CLONE_NEWUSER
      Linux 5.17.5
      arm64: dts: qcom: add IPA qcom,qmp property
      io_uring: fix leaks on IOPOLL and CQE_SKIP
      ASoC: SOF: topology: cleanup dailinks on widget unload
      spi: atmel-quadspi: Fix the buswidth adjustment between spi-mem and controller
      jbd2: fix a potential race while discarding reserved buffers after an abort
      ext4: update the cached overhead value in the superblock
      ext4: force overhead calculation if the s_overhead_cluster makes no sense
      ext4: fix overhead calculation to account for the reserved gdt blocks
      ext4, doc: fix incorrect h_reserved size
      ext4: limit length to bitmap_maxbytes - blocksize in punch_hole
      ext4: fix use-after-free in ext4_search_dir
      ...
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- include/linux/memcontrol.h
+++ include/linux/memcontrol.h
@@ -1004,6 +1004,7 @@ static inline unsigned long lruvec_page_state_local(struct lruvec *lruvec,
 }
 
 void mem_cgroup_flush_stats(void);
+void mem_cgroup_flush_stats_delayed(void);
 
 void __mod_memcg_lruvec_state(struct lruvec *lruvec, enum node_stat_item idx,
 			      int val);
@@ -1450,6 +1451,10 @@ static inline unsigned long lruvec_page_state_local(struct lruvec *lruvec,
 }
 
 static inline void mem_cgroup_flush_stats(void)
+{
+}
+
+static inline void mem_cgroup_flush_stats_delayed(void)
 {
 }
 
